1b4613c94c5d1aabb64941dbf5396a7239a7d943
Angela Mumba <malaika.mumba@gmail.com>
2019-07-09 19:23:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,14 +45,16 @@ end
 
   patch '/articles/:id' do
     @article = Article.find(params[:id])
+    
     @article.update(params[:article])
-    redirect to "/articles/#{@article.id}"
+    @article.save 
+    redirect "/articles/#{@article.id}"
   end
 
   delete '/articles/:id' do
     article = Article.find(params[:id])
     article.destroy 
-    redirect to '/articles'
+    redirect '/articles'
   end  
 end
 
